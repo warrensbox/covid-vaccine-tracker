a53c5740fbdcf10fcc75589fd9bd15ca26421c76
warren.veerasingam <warren.veerasingam@gmail.com>
2021-04-05 22:05:48 -0500

    added lambda state

--- test_lambda.sh
+++ test_lambda.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+if [ -z "$1" ]
+  then
+    echo "No argument supplied"
+fi
+
+STATE=$1
+
 aws --region us-east-1 lambda invoke \
---function-name covid-vaccine-iowa \
+--function-name covid-vaccine-$STATE \
 text.json
